68fbc742b6f5b6c667153784b60e11cf1784e1bb
Alberto Gualis <agualis@gmail.com>
2023-03-14 09:38:15 +0100

    Test bundle with local SDK

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-import { ISOLATED_CONST } from '@balancer-labs/sor';
+import { ISOLATED_CONST } from '@balancer-labs/sdk';
 
 console.log('ISOLATED_CONST:', ISOLATED_CONST);
 
