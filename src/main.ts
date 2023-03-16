4dac25c9d34a515555f8dfdc6f58ac05908235a6
Alberto Gualis <agualis@gmail.com>
2023-03-16 14:45:57 +0100

    Add debug balancer SDK import

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,19 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import { ISOLATED_CONST } from '@balancer-labs/sdk';
+// import { ISOLATED_CONST, BalancerSDK } from '@balancer-labs/sdk';
 
 console.log('ISOLATED_CONST:', ISOLATED_CONST);
 
-// console.log('WeightedPool:', WeightedPool);
+// //Uncomment to see how importing balancer SDK affects the bundle size
+// export const balancer = new BalancerSDK({
+//   network: 5,
+//   rpcUrl: 'test-url',
+//   customSubgraphUrl: 'subgraphurl',
+// });
+//
+// console.log('balancer', balancer);
+
 // console.log('WeightedMaths:', SOR);
 
 createApp(App).mount('#app')
